0a454b0f0aec9ea92fdbdce31f5aefe16acbb9bb
Isaac Husemann <isaac.husemann@gmail.com>
2024-06-17 09:24:57 -0500

    remove web app

--- apps/web/app/schema.ts
+++
@@ -1,9 +0,0 @@
-import { string, index, int32 } from "azure-ai-search-orm";
-
-export const realEstate = index("realestate-us-sample-index", {
-  listingId: string("listingId").key(),
-  beds: int32("beds").filterable().sortable().facetable().notNull(),
-  baths: int32("baths").filterable().sortable().facetable().notNull(),
-  description: string("description").searchable().notNull(),
-  sqft: int32("sqft").filterable().sortable().facetable().notNull(),
-});
