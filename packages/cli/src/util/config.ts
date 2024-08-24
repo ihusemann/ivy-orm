1258a97629db8c228796b27050a774f761a8f865
Isaac Husemann <isaac.husemann@gmail.com>
2024-08-23 19:34:02 -0500

    cleanup unused config functions

--- packages/cli/src/util/config.ts
+++ packages/cli/src/util/config.ts
@@ -1,9 +1,5 @@
 import { type TokenCredential } from "@azure/identity";
-import { cosmiconfig } from "cosmiconfig";
-import { z, ZodError } from "zod";
-import chalk from "chalk";
-
-const explorer = cosmiconfig("ivy-kit");
+import { z } from "zod";
 
 function isTokenCredential(credential: any): credential is TokenCredential {
   try {
@@ -23,50 +19,4 @@ export type Config = {
   schema?: string;
   endpoint: string;
   credential: TokenCredential;
-};
-
-export const safeRegister = async () => {
-  const { register } = await import("esbuild-register/dist/node");
-  let res: { unregister: () => void };
-  try {
-    res = register({
-      format: "cjs",
-      loader: "ts",
-    });
-  } catch {
-    // tsx fallback
-    res = {
-      unregister: () => {},
-    };
-  }
-  return res;
-};
-
-export const getConfig = async (cwd: string): Promise<Config> => {
-  // console.log("HERE!");
-  // const { unregister } = await safeRegister();
-  // console.log("SAFE REGISTER");
-  const result = await explorer.search(cwd);
-  // console.log("HERE");
-
-  if (!result)
-    throw new Error(
-      `Configuration is missing.  Please add ${chalk.green("ivy-kit.config.ts")} to your project root.`
-    );
-
-  try {
-    const config = configSchema.parse(result.config.default);
-
-    return config;
-  } catch (e) {
-    if (e instanceof ZodError) {
-      const message = `Invalid configuration at '${result.filepath}'.\nField(s) ${Object.keys(
-        e.flatten().fieldErrors
-      ).join(", ")} are invalid.`;
-      throw new Error(chalk.red(message));
-    }
-    throw new Error("Could not parse configuration.");
-  } finally {
-    // unregister();
-  }
 };
