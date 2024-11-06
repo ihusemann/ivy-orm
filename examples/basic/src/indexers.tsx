f449c3e50fce1d5192bca0304ab3804109dbb4ff
Isaac Husemann <isaac.husemann@gmail.com>
2024-11-05 19:30:35 -0600

    cleanup

--- examples/basic/src/indexers.tsx
+++
@@ -1,7 +0,0 @@
-import { indexer } from "azure-ai-search-orm";
-import { realEstate } from "./indexes";
-
-export const realEstateIndexer = indexer("real-estate-indexer", {
-  dataSourceName: "realestate-us-sample",
-  targetIndex: realEstate,
-});
